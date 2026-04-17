127ba4b158a5b3b4d5332a1069f048db05e84939
ralphiedawg <russellwpage@gmail.com>
2026-04-17 10:30:22 -0400

    Fix bug where heartbeats only generate timestamp once

--- src/media/Heartbeat.py
+++ src/media/Heartbeat.py
@@ -1,13 +1,12 @@
 from Communication import Communication
 import json
 import time
-from Client import Client
+#from Client import Client
 
 class Heartbeat(Communication):
     device_id = 0
     def __init__(self, device):
         self.device = device
-        self.timestamp = str(time.time())
         self.device_id = self.device.id
         self.playStatus = self.device.playStatus()
     def heartbeat(self):
@@ -15,7 +14,7 @@ class Heartbeat(Communication):
                 {
                     'type': 'heartbeat',
                     'device-id': self.device_id,
-                    'timestamp': self.timestamp,
+                    'timestamp': str(time.time()),
                     'device-type': self.device.deviceType,
                     'playStatus': self.playStatus,
                 }
@@ -34,7 +33,9 @@ class Heartbeat(Communication):
             )
         )
 
+"""
 if __name__ == "__main__":
     c1 = Client()
     h1 = Heartbeat(c1)
     print(h1.heartbeat())
+"""
