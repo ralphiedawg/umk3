ec6dcdcf5ddb417d026295bd8ca35d4a292b3e4e
ralphiedawg <russellwpage@gmail.com>
2026-05-07 11:13:08 -0400

    feat: arbiter to determine whether heartbeat is new, returns id of changed device

--- src/media/recency_arbitration/RecencyArbiter.py
+++ src/media/recency_arbitration/RecencyArbiter.py
@@ -1,17 +1,47 @@
 import json
 
-from src.media.recency_arbitration.MacOSWrapper import MacOSWrapper
+class RecencyArbiter:
+    def __init__(self, heartbeats=None):
+        self.heartbeats = {}
+        if heartbeats:
+            self.heartbeats = self._enumerate_givens(heartbeats)
+    """
+    {"type": "heartbeat", "device-id": 1, "timestamp": "1777998089.264807", "device-type": "client", "play-status": "not_playing"}
+    {"type": "heartbeat", "device-id": 1, "timestamp": "1777998099.360051", "device-type": "client", "play-status": "playing"}
+    """
+    def _pull_id(self, heartbeat) -> int:
+        data = json.loads(heartbeat)
+        return data['device-id']
+    def _enumerate_givens(self, heartbeats: list) -> dict:
+        final = {}
+        for heartbeat in heartbeats:
+            device_id = self._pull_id(heartbeat)
+            final[device_id] = heartbeat
+        return final
+    def arbitrate(self, raw_heartbeat) -> int:
+        """Check heartbeat against stored list of old heartbeats, returns value of changed client, 0 if not"""
+        new = json.loads(raw_heartbeat)
+        device_id = self._pull_id(raw_heartbeat)
 
-"""{"type": "heartbeat", "device-id": 1, "timestamp": "1777911430.6075552", "device-type": "client", "play-status": "not_playing"}"""
-class RecencyArbiter():
-    def __init__(self, heartbeats):
-        self.heartbeats = heartbeats
-        self.old_beats = [] # Last recieved heartbeat for every client
-    async def determine_active(self) -> int:
-        changed = []
-        for heartbeat in self.heartbeats:
-            heartbeat = json.loads(heartbeat)
-            id = heartbeat['device-id']
-            current = heartbeat['play-status']
-            old = [item for item in self.old_beats if item['device-id'] == id]
-        return 1
+        raw_old = self.heartbeats[device_id]
+        if device_id  not in self.heartbeats:
+            self.heartbeats[device_id] = raw_heartbeat
+            return device_id
+        old = json.loads(raw_old)
+
+        if (
+            (old['play-status'] != new['play-status']) 
+            and (float(old['timestamp']) < float(new['timestamp']))
+        ):
+            self.heartbeats[device_id] = raw_heartbeat
+
+            return device_id
+        return 0
+
+if __name__ == '__main__':
+    beats = [
+        '{"type": "heartbeat", "device-id": 3, "timestamp": "1777998089.264807", "device-type": "client", "play-status": "not_playing"}',
+        '{"type": "heartbeat", "device-id": 5, "timestamp": "1777998089.264807", "device-type": "client", "play-status": "not_playing"}',
+        '{"type": "heartbeat", "device-id": 1, "timestamp": "1777998099.360051", "device-type": "client", "play-status": "playing"}'
+    ]
+    c = RecencyArbiter(beats)
