d617d32175d1ae435545f7192c31542ac5f0646b
ralphiedawg <russellwpage@gmail.com>
2026-05-12 12:02:20 -0400

    fix: WrapperHelper.py && change: skip5 gesture

--- src/media/recency_arbitration/WrapperHelper.py
+++ src/media/recency_arbitration/WrapperHelper.py
@@ -6,8 +6,8 @@ class WrapperHelper:
     def agnostic_media_status():
         os = platform.system()
         if os == 'Darwin':
+            from src.media.recency_arbitration.MacOSWrapper import MacOSWrapper
             if MacOSWrapper.is_playing():
-                from src.media.recency_arbitration.MacOSWrapper import MacOSWrapper
                 return True
             return False
         elif os == 'Windows':
